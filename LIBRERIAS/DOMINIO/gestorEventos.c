24f7709921edc45e48c562327a5f507123aa14bb
Natrox2017 <natroxsolosteam1942@gmail.com>
2026-06-05 13:29:04 -0300

    SE AGREGARON LAS ESTRUCTURAS SOLICITADAS Y EL PROTOTIPADO DE ALGUNAS FUNCIONES

--- LIBRERIAS/DOMINIO/gestorEventos.c
+++ LIBRERIAS/DOMINIO/gestorEventos.c
@@ -1,1 +1,65 @@
 #include "gestorEventos.h"
+#include "../Librerias Externas/scanner.h"
+
+typedef struct	stPuntaje
+{
+	float valorPuntaje;
+	int esValido;
+} Puntaje;
+
+typedef struct stFechaLanzamiento
+{
+	int dia;
+	int mes;
+	int anio;
+	int esValido;
+} FechaLanzamiento;
+
+typedef struct stJuego
+{
+	int idJuego;
+	char nombre[50];
+	char estudio[50];
+	char genero[50];
+} Juego;
+
+typedef struct stCategoria
+{
+	int idCategoria;
+	char nombre[50];
+} Categoria;
+
+typedef struct stNominacion
+{
+	int idNominacion;
+	Categoria categoria;
+	Juego juego;
+	Puntaje puntaje;
+	FechaLanzamiento fecha;
+}Nominacion;
+
+void abrirArchivoLimpio(char nombreArchivo[])
+{
+	FILE* fp;
+	fp = fopen(nombreArchivo, "wb");
+
+	if (!fp)
+	{
+		printf("Error, no se logro abrir el archivo.\n");
+		return 0;
+	}
+
+	printf("El archivo se creo y/o abrio vacio.\n");
+
+	fclose(fp);
+}
+
+void cargarArchivoConNominaciones()
+{
+
+}
+
+Nominacion cargarNuevaNominacion(Nominacion aux)
+{
+	
+}
--- mainTpFinal.c
+++ mainTpFinal.c
@@ -1,9 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "LIBRERIAS/INTERFAZ_DE_USUARIO/menus.h"
+
 
 
 int main()
 {
-	printf("Hola Mundo");
+	printf("Hola Mundo.\n");
+	menuPrincipal();
 	return 0;
 }
