32191ba183a550ad7db0305c1078e42896d065ec
Natrox2017 <natroxsolosteam1942@gmail.com>
2026-06-06 14:41:21 -0300

    se empezo la implementacion de la funcion mostrarGOTYS en la capa de servicio

--- LIBRERIAS/SERVICIO/servicio.c
+++ LIBRERIAS/SERVICIO/servicio.c
@@ -1,1 +1,20 @@
 #include "servicio.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrarGOTYS(char nombreArchivo)
+{
+	FILE* fp;
+	fp = fopen(nombreArchivo, "rb");
+
+	if (!fp)
+	{
+		printf("Error no se pudo abrir el archivo.\n");
+		return;
+	}
+
+
+
+
+	fclose(fp);
+}
